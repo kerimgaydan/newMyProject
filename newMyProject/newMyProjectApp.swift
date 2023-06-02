45a61267de248913af6ba58ba84f7dbab60bc4a7
kerimgaydan <mkerimgaydan@gmail.com>
2023-06-02 22:24:38 +0300

    Initial Commit

---
+++ newMyProject/newMyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  newMyProjectApp.swift
+//  newMyProject
+//
+//  Created by Kerim Gaydan on 2.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct newMyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
