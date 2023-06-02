d9f4f863dd8dd55055d54b0c1b922d772c10365d
kerimgaydan <mkerimgaydan@gmail.com>
2023-06-02 22:28:00 +0300

    commit 1

--- newMyProject/ContentView.swift
+++ newMyProject/ContentView.swift
@@ -8,17 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                    Text("Count: \(count)")
+                        .font(.largeTitle)
+                        .padding()
+                    
+                    Button(action: {
+                        count += 1
+                    }) {
+                        Text("Increment")
+                            .font(.title)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    
+                    Button(action: {
+                        count = 0
+                    }) {
+                        Text("Reset")
+                            .font(.title)
+                            .padding()
+                            .background(Color.red)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }
+            }
         }
-        .padding()
-    }
-}
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
